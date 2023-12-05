6bf7526730e9d898d91bd720f72b5ec5f4ecff1a
checkiamsiam <issiam02415@gmail.com>
2023-12-05 16:12:17 +0600

    error solve

--- src/components/sectionComponents/HomeCategories/HomeCategoriesMap.tsx
+++ src/components/sectionComponents/HomeCategories/HomeCategoriesMap.tsx
@@ -25,7 +25,7 @@ const HomeCategoriesMap = () => {
   return (
     <div>
       <div className="flex flex-wrap md:gap-10 gap-5 justify-center ">
-        {data?.categories.slice(0, 14).map((cat, i) => (
+        {data?.categories?.slice(0, 14).map((cat, i) => (
           <CategoryLinkCard key={cat.id} link={`/categories/${cat.id}`} img={cat.icon} title={cat.title} />
         ))}
       </div>
