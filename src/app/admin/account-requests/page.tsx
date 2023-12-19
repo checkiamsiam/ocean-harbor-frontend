58bd6fb915a10753fa754b08dd943936db78b969
checkiamsiam <issiam02415@gmail.com>
2023-12-20 01:04:27 +0600

    Update Manage Customer Page and Account Request API

--- src/app/admin/account-requests/page.tsx
+++ src/app/admin/account-requests/page.tsx
@@ -1,18 +1,17 @@
 "use client";
 import GAActionBar from "@/components/ui/GAActionBar";
 import GABreadCrumb from "@/components/ui/GABreadcrumb";
-import GAButton from "@/components/ui/GAButton";
 import GATable from "@/components/ui/GATable";
 import { useDebounced } from "@/hooks/useDebounced";
 import { Link } from "@/lib/router-events";
-import { useGetCustomersQuery, useUpdateCustomerMutation } from "@/redux/features/user/userApi";
-import { CustomerStatus } from "@/types/ApiResponse";
-import { Button, Input, Switch, TableColumnProps, Tooltip, message } from "antd";
+import { useDeclineAccountRequestMutation, useGetAccountRequestsQuery } from "@/redux/features/accountRequest/accoutRequestApi";
+import { Button, Input, Modal, TableColumnProps, Tooltip, message } from "antd";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { AiOutlineReload } from "react-icons/ai";
+const { confirm } = Modal;
 
-const ManageCustomerPage = () => {
+const AccountRequestsPage = () => {
   const { data: session } = useSession();
   const query: Record<string, any> = {};
 
@@ -21,12 +20,10 @@ const ManageCustomerPage = () => {
   const [sortBy, setSortBy] = useState<string>("");
   const [sortOrder, setSortOrder] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const [statusFilter, setStatusFilter] = useState<string[] | null>(null);
 
   query["limit"] = size;
   query["page"] = page;
   query["sort"] = !!sortBy && !!sortOrder && sortOrder === "asc" ? sortBy : sortOrder === "desc" ? `-${sortBy}` : undefined;
-  query["status"] = statusFilter ? statusFilter.join(",") : undefined;
 
   const debouncedTerm = useDebounced({
     searchQuery: searchTerm,
@@ -37,8 +34,9 @@ const ManageCustomerPage = () => {
     query["searchKey"] = debouncedTerm;
   }
 
-  const [updateCustomer] = useUpdateCustomerMutation();
-  const { data, isLoading } = useGetCustomersQuery(
+  const [acceptCustomer] = useDeclineAccountRequestMutation();
+
+  const { data, isLoading } = useGetAccountRequestsQuery(
     { params: { ...query } },
     {
       refetchOnMountOrArgChange: true,
@@ -46,7 +44,7 @@ const ManageCustomerPage = () => {
     }
   );
 
-  const customers = data?.customers;
+  const requests = data?.requests;
   const meta = data?.meta;
 
   const columns: TableColumnProps<any>[] = [
@@ -67,34 +65,19 @@ const ManageCustomerPage = () => {
       dataIndex: "phone",
     },
     {
-      title: "Status",
-      filters: [
-        { text: "Active", value: CustomerStatus.active },
-        { text: "Disabled", value: CustomerStatus.disabled },
-      ],
-      filterMultiple: false,
-      render: function (data) {
-        return (
-          <div className="flex justify-center">
-            <Switch size="small" defaultChecked={data.status === CustomerStatus.active} onChange={(e) => handleSwitchStatus(e, data.id)} />
-          </div>
-        );
-      },
-    },
-    {
       title: "Action",
       dataIndex: "id",
       render: function (data: string) {
         return (
           <div className="flex justify-center items-center gap-5">
-            <Tooltip title="View Details" key={`ec-${data}`}>
-              <Link href={`/admin/manage-customer/details/${data}`} className="cursor-pointer text-blue-400 underline">
-                view
-              </Link>
+            <Tooltip title="Decline Request" key={`dar-${data}`}>
+              <span onClick={() => showDeclineAccountRequest(data)} className="cursor-pointer text-blue-400 underline">
+                decline
+              </span>
             </Tooltip>
-            <Tooltip title="Edit Customer" key={`vdc-${data}`}>
-              <Link href={`/admin/manage-customer/edit/${data}`} className="cursor-pointer text-blue-400 underline">
-                edit
+            <Tooltip title="Accept Request" key={`aar-${data}`}>
+              <Link href={`/admin/account-requests/accept/${data}`} className="cursor-pointer text-blue-400 underline">
+                accept
               </Link>
             </Tooltip>
           </div>
@@ -103,12 +86,37 @@ const ManageCustomerPage = () => {
     },
   ];
 
+  const handleDeclineAccountRequest = async (id: string) => {
+    message.loading("Declining Request.....");
+    try {
+      const res = await acceptCustomer({ id }).unwrap();
+      if (!!res) {
+        message.destroy();
+        message.success("Your request to decline registration has been sent successful");
+      }
+    } catch (err: any) {
+      message.destroy();
+      message.warning("Failed to Decline! try again");
+    }
+  };
+
+  const showDeclineAccountRequest = (data: string) => {
+    confirm({
+      title: "Are you sure decline this registration request?",
+      content: "Press 'Ok' to decline this order or 'Cancel' to back to previous page",
+      okText: "Yes",
+      okType: "danger",
+      cancelText: "No",
+      onOk() {
+        handleDeclineAccountRequest(data);
+      },
+    });
+  };
   const onPaginationChange = (page: number, pageSize: number) => {
     setPage(page);
     setSize(pageSize);
   };
   const onTableChange = (pagination: any, filter: any, sorter: any) => {
-    setStatusFilter(filter.status);
     const { order, field } = sorter;
     if (!(order === undefined || field === undefined)) {
       setSortBy(field as string);
@@ -116,37 +124,17 @@ const ManageCustomerPage = () => {
     }
   };
 
-  const handleSwitchStatus = async (checked: boolean, id: string) => {
-    message.loading(checked ? "Making Active..." : "Making Disable...");
-    try {
-      const res = await updateCustomer({
-        id,
-        data: {
-          status: checked ? CustomerStatus.active : CustomerStatus.disabled,
-        },
-      }).unwrap();
-      if (!!res) {
-        message.destroy();
-        message.success(`Your request to ${checked ? "active" : "disable"} customer has been sent successful`);
-      }
-    } catch (err: any) {
-      message.destroy();
-      message.warning(`Failed to ${checked ? "active" : "disable"} customer! try again`);
-    }
-  };
-
   const resetFilters = () => {
     setSortBy("");
     setSortOrder("");
     setSearchTerm("");
-    setStatusFilter(null);
   };
 
   return (
     <div>
-      <GAActionBar title="Manage Customer">
+      <GAActionBar title="Account Requests">
         <div className="flex md:flex-row flex-col gap-5 md:gap-0  justify-between items-center">
-          <GABreadCrumb items={[{ label: "Accounts" }, { label: "Manage Customer" }]} />
+          <GABreadCrumb items={[{ label: "Accounts" }, { label: "Requests" }]} />
           <div className="w-full md:w-1/4">
             <Input
               type="text"
@@ -160,9 +148,6 @@ const ManageCustomerPage = () => {
           </div>
         </div>
         <div>
-          <Link href="/admin/manage-customer/create">
-            <GAButton type="primary">Add Customer</GAButton>
-          </Link>
           {(!!sortBy || !!sortOrder || !!searchTerm) && (
             <Button style={{ margin: "0px 5px" }} type="primary" onClick={resetFilters}>
               <AiOutlineReload />
@@ -174,7 +159,7 @@ const ManageCustomerPage = () => {
       <GATable
         loading={isLoading}
         columns={columns}
-        dataSource={customers}
+        dataSource={requests}
         pageSize={size}
         totalPages={meta?.total}
         showSizeChanger={true}
@@ -186,4 +171,4 @@ const ManageCustomerPage = () => {
   );
 };
 
-export default ManageCustomerPage;
+export default AccountRequestsPage;
--- src/redux/features/accountRequest/accoutRequestApi.tsx
+++ src/redux/features/accountRequest/accoutRequestApi.tsx
@@ -1,4 +1,6 @@
 import { baseApi } from "@/redux/baseApi";
+import { tagTypes } from "@/redux/tag-types";
+import { IMeta, IQuery } from "@/types";
 import { AccountRequest } from "@/types/ApiResponse";
 
 const account_request = "/account-request";
@@ -7,15 +9,64 @@ export type IAccReq = Omit<AccountRequest, "id">;
 
 export const accountRequestApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
+    getAccountRequests: builder.query<{ requests: AccountRequest[]; meta: IMeta }, { params?: IQuery }>({
+      query: (arg) => ({
+        url: account_request,
+        method: "GET",
+        params: arg?.params,
+      }),
+      transformResponse: (response: AccountRequest[], meta: IMeta) => {
+        return {
+          requests: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.user],
+    }),
+
+    getSingleAccountRequest: builder.query<{ request: AccountRequest }, { id: string; params?: IQuery }>({
+      query: (arg) => ({
+        url: account_request + "/" + arg?.id,
+        method: "GET",
+        params: arg?.params,
+      }),
+      transformResponse: (response: AccountRequest) => {
+        return {
+          request: response,
+        };
+      },
+      providesTags: [tagTypes.user],
+    }),
     accountRequest: builder.mutation({
       query: (arg: IAccReq) => ({
         url: account_request + "/create",
         method: "POST",
         data: arg,
       }),
+      invalidatesTags: [tagTypes.user],
+    }),
+    acceptAccountRequest: builder.mutation({
+      query: (arg: { id: string; password: string }) => ({
+        url: account_request + "/accept" + "/" + arg.id,
+        method: "PATCH",
+        data: { password: arg.password },
+      }),
+      invalidatesTags: [tagTypes.user],
+    }),
+    declineAccountRequest: builder.mutation({
+      query: (arg: { id: string }) => ({
+        url: account_request + "/delete" + "/" + arg.id,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.user],
     }),
   }),
 });
 
-
-export const { useAccountRequestMutation } = accountRequestApi;
+export const {
+  useAccountRequestMutation,
+  useAcceptAccountRequestMutation,
+  useDeclineAccountRequestMutation,
+  useGetAccountRequestsQuery,
+  useGetSingleAccountRequestQuery,
+} = accountRequestApi;
