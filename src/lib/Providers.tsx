563b627e2ce3bfe703f614328a786eb005dec93f
checkiamsiam <issiam02415@gmail.com>
2023-12-20 18:54:59 +0600

    Refactor Redux store and hooks

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,17 +1,22 @@
 "use client";
-import { store } from "@/redux/store";
+import { AppStore, makeStore } from "@/redux/store";
 import { Session as NextAuthSession } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+import { ReactNode, useRef } from "react";
 import { Provider } from "react-redux";
 import StyledComponentsRegistry from "./AntdRegistry";
 import BrowserActivityProvider from "./BrowserActivityProvider";
 
-const Providers = ({ children, session  }: { children: ReactNode; session: NextAuthSession }) => {
+const Providers = ({ children, session }: { children: ReactNode; session: NextAuthSession }) => {
+  const storeRef = useRef<AppStore>();
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore();
+  }
   return (
     <SessionProvider session={session}>
       <BrowserActivityProvider>
-        <Provider store={store}>
+        <Provider store={storeRef.current}>
           <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
         </Provider>
       </BrowserActivityProvider>
