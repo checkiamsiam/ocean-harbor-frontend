669bdd51a51543d76422441f1b668e0d0a34eb44
checkiamsiam <issiam02415@gmail.com>
2023-12-22 22:04:21 +0600

    Refactor HomeBrandsCarouselMap, HomeCategoriesMap, Brands, and CategoriesMap components

--- src/components/sectionComponents/HomeCategories/HomeCategoriesMap.tsx
+++ src/components/sectionComponents/HomeCategories/HomeCategoriesMap.tsx
@@ -1,16 +1,29 @@
+"use client";
 import CategoryLinkCard from "@/components/common/CategoryLinkCard";
-import { getCategory } from "@/redux/features/category/categoryApi";
+import { useGetCategoriesQuery } from "@/redux/features/category/categoryApi";
+import { Skeleton } from "antd";
 
-const HomeCategoriesMap = async () => {
-  const data = await getCategory({});
+const HomeCategoriesMap = () => {
+  const { data, isLoading } = useGetCategoriesQuery({
+    params: {
+      limit: 14,
+      page: 1,
+    },
+  });
   return (
     <div>
-      <div className="flex flex-wrap md:gap-10 gap-5 justify-center ">
-        {data.categories &&
-          data?.categories
-            ?.slice(0, 14)
-            ?.map((cat, i) => <CategoryLinkCard key={cat.id} link={`/categories/${cat.id}`} img={cat.icon} title={cat.title} />)}
-      </div>
+      {isLoading ? (
+        <div>
+          <Skeleton active />
+        </div>
+      ) : (
+        <div className="flex flex-wrap md:gap-10 gap-5 justify-center ">
+          {data?.categories &&
+            data?.categories
+              ?.slice(0, 14)
+              ?.map((cat, i) => <CategoryLinkCard key={cat.id} link={`/categories/${cat.id}`} img={cat.icon} title={cat.title} />)}
+        </div>
+      )}
     </div>
   );
 };
